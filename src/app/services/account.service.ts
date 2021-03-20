13d5a1d2284c54b77b51c6616518d33770637177
Kapil Ghimire <Kapil.Ghimire@unilever.com>
2021-03-20 09:43:56 +0530

    Fixed account service error

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
@@ -11,17 +12,13 @@ import { Customer } from '../modals/customer';
  })
  // Use 'API_URL' from environment.ts
 export class AccountService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
   // Function to get User's account details using email with JWT authentication token
   // and error handling using erro-handler.ts
-  getAccountDetails(payload: string): Observable<Customer> {
-
-    return of({"id": "1",
-    "firstName": "bruno",
-    "lastName": "mars",
-    "email": "bruno@email.com",
-    "password": "bruno"});
+  getAccountDetails(email: string): Observable<Customer> {
+   return  this.httpClient.get<Customer>("http://localhost:8761/customers?"+email);
+    
   }
 
   // Function to update or create new User's account details with JWT authentication token
