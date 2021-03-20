b749da656ed64f13a7484fcb7cfd4a55d4866cf2
github_Manish <m.karn2011@gmail.com>
2021-03-20 10:32:24 +0530

    common api url from environment file

--- src/app/services/login-register.service.ts
+++ src/app/services/login-register.service.ts
@@ -1,19 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 // Use 'API_URL' from environment.ts
 export class LoginRegisterService {
-  baseUrl="http://localhost:8761/auth/";
+  baseUrl=environment.API_URL;
   constructor(private http:HttpClient) {}
 
   // Function to send login data to the backend
   // and error handling using erro-handler.ts
   login(payload: object): Observable<any> {
-return this.http.post(this.baseUrl+'login',payload);
+return this.http.post(this.baseUrl+'/auth/login',payload);
    // return of({"access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"});
   }
 
